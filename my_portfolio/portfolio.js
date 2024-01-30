852dc7215550af21c5f50e9f1594538f7d32bdf8
Khun01 <152861024+Khun01@users.noreply.github.com>
2024-01-31 00:02:14 +0800

    Bago uli

--- my_portfolio/portfolio.js
+++ my_portfolio/portfolio.js
@@ -1,19 +1,20 @@
 /*-------Active Link-------*/
 
-const sections = document.querySelectorAll(".section[id]");
+const sections = document.querySelectorAll("section[id]");
 function scrollActive(){
     const scrollY = window.scrollY;
 
     sections.forEach((current) => {
         const sectionHeight = current.offsetHeight,
+
             sectionTop = current.offsetTop - 50,
             sectionId = current.getAttribute("id");
+            
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document
                 .querySelector(".nav-menu a[href*=" + sectionId + "]")
                 .classList.add("active-link");
-        }
-        else{
+        } else{
             document   
                 .querySelector(".nav-menu a[href*=" + sectionId + "]")
                 .classList.remove("active-link");
